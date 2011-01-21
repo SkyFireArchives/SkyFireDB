db961fd3016af49d5c4f0d063d1ce6c7f51c1640
SkyFire <nonya@effing_beeezwax.gtfo>
2011-01-21 15:48:02 -0600

    Remove Christmas trees
    Signed-off-by: SkyFire <nonya@effing_beeezwax.gtfo>

---
+++ update_packs/21_01_2011_DESPAWN_XMAS_TREES.sql
@@ -0,0 +1,13 @@
+-- Despawn xmas trees
+DELETE FROM `gameobject` WHERE `id`=178647;
+DELETE FROM `gameobject` WHERE `id`=206976;
+DELETE FROM `gameobject` WHERE `id`=178558;
+DELETE FROM `gameobject` WHERE `id`=178425;
+DELETE FROM `gameobject` WHERE `id`=178557;
+DELETE FROM `gameobject` WHERE `id`=178667;
+DELETE FROM `gameobject` WHERE `id`=178668;
+DELETE FROM `gameobject` WHERE `id`=178805;
+DELETE FROM `gameobject` WHERE `id`=206104;
+DELETE FROM `gameobject` WHERE `id`=206077;
+DELETE FROM `gameobject` WHERE `id`=168460;
+
