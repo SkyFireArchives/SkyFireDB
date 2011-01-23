3138bb875e31f43aee4db6e9a316d83c20a3ee03
AriDEV <ari@nightandmagic.cz>
2011-01-23 13:33:39 +0100

    Deeprun Tram Instance Teleport [FIX]

---
+++ update_packs/23_01_2011_Deeprun_Tram_Instance_Teleport.sql
@@ -0,0 +1,2 @@
+-- Deeprun Tram Instance Teleport
+UPDATE areatrigger_teleport SET target_position_x=-8358.86, target_position_y=516.954, target_position_z=84.41, target_orientation=2.259887 WHERE id=2171;
