17b99f6ad391ce703748c8c991fb3be889a4e64b
AriDEV <ari@nightandmagic.cz>
2011-01-24 18:14:40 +0100

    Goblin Shaman Lightning Bold [FIX]

--- update_packs/24_01_2011_Goblin_Shaman_Lightning_Bolt.sql
+++ update_packs/24_01_2011_Goblin_Shaman_Lightning_Bolt.sql
@@ -1,4 +1,4 @@
 -- Goblin Shaman Lightning Bolt FIX
-DELETE FROM playercreateinfo_spell WHERE race=9 AND spell=585;
-INSERT INTO playercreateinfo_spell (race, class, Spell, Note) VALUES (9, 5, 585, 'Lightning Bolt');
+DELETE FROM playercreateinfo_spell WHERE race=9 AND spell=403;
+INSERT INTO playercreateinfo_spell (race, class, Spell, Note) VALUES (9, 7, 403, 'Lightning Bolt');
 -- Fix By Ari
