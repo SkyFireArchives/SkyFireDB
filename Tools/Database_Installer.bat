c1e761f049e1c6391ac76aa7c4947ab504b62182
AlterEgo <nonya@effing_beeezwax.gtfo>
2011-02-13 01:56:40 -0600

    Fix a typo.
    Signed-off-by: AlterEgo <nonya@effing_beeezwax.gtfo>

--- Tools/Database_Installer.bat
+++ Tools/Database_Installer.bat
@@ -76,8 +76,9 @@ FOR %%C IN (%devsql%\*.sql) DO (
 	ECHO Successfully imported %%~nxC
 )
 ECHO Done.
-ECHO Your current database is now SkyFireDB 403.02_13_2011 Rev 8
-ECHO Changeset 8. You don't need to apply any updates.
+ECHO.
+ECHO Your current database is now SkyFireDB 403_02_13_2011 Rev 8.
+ECHO Changeset 8 You don't need to apply any updates.
 ECHO.
 ECHO.
 ECHO.
