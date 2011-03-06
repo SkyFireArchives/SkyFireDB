b3e11168ba3722903644337cd6f19ee219793cfb
AriDEV <ari@nightandmagic.cz>
2011-03-06 11:53:27 -0800

    Minnor Fix At Goblin Assassin

--- Updates/64_world_creature.sql
+++ Updates/64_world_creature.sql
@@ -1,3 +1,3 @@
 -- Goblin Assassin (Movement)
-UPDATE creature SET spawntimesecs=300, spawndist=5, MovementType=1 WHERE entry=50039;
+UPDATE creature SET spawntimesecs=300, spawndist=5, MovementType=1 WHERE id=50039;
 -- Fix By Ari.
