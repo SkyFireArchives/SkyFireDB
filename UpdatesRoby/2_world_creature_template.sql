cb9ed498ead42ba895e1f8cd6fca79df42e71d14
Robert <robert_n6@hotmail.com>
2011-03-11 05:53:01 +0100

    fix for http://skyfireemu.lighthouseapp.com/projects/69418/tickets/53-quest-destroy-the-legion

---
+++ UpdatesRoby/2_world_creature_template.sql
@@ -0,0 +1,7 @@
+--Destroy the legion--
+--Roaming Felguard--
+UPDATE `creature_template` SET `KillCredit1`=44032 WHERE `entry`=6115
+--Searing Infernal--
+UPDATE `creature_template` SET `KillCredit1`=44032 WHERE `entry`=6073
+
+--Fix by Robydj--
