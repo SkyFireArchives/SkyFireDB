af492ae3eee6caaa5e7cd8a79d8c3e24bb57cca3
Hammerstorm <wartactics00@gmail.com>
2011-03-16 10:51:54 -0500

    -fixed bad commit, sorry

--- UpdatesRoby/1_world_creature_template.sql
+++
@@ -1,7 +0,0 @@
---A Little Diversion--
---Kolkar Stormer--
-UPDATE `creature_template` SET `KillCredit1`=34614 WHERE `entry`=3273
---Kolkar Wrangler--
-UPDATE `creature_template` SET `KillCredit1`=34614 WHERE `entry`=3272
-
---Fix by Robydj--
