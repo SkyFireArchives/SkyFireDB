8f13eb416c5b4226e044120aabd675c6c8d262ed
AriDEV <michalik.andrej@centrum.sk>
2011-03-17 18:45:36 +0100

    [OBJECT] Object Flags For Quest Overlord Mok'Morokk's Concern (27424)

--- Updates/188_world_quest_27424.sql
+++ Updates/188_world_quest_27424.sql
@@ -7,3 +7,11 @@ INSERT INTO gameobject_loot_template (entry, item, ChanceOrQuestChance) VALUES
 (19906, 5836, -100), -- Mok'Morokk's Strongbox
 (19905, 5835, -100), -- Mok'Morokk's Grog
 (19904, 5834, -100); -- Mok'Morokk's Snuff
+
+-- Gameobject Flags
+UPDATE gameobject_template SET flags=4 WHERE entry IN 
+(
+19904, -- Mok'Morokk's Snuff
+19905, -- Mok'Morokk's Grog
+19906 -- Mok'Morokk's Strongbox
+);
