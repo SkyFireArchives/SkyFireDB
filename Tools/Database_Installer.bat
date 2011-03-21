e4d8569e25e285ab4f39fc0c49f20bcff7f915ca
AriDEV <michalik.andrej@centrum.sk>
2011-03-21 17:21:36 +0100

    DB/Quest: Good-Fer-Nothin' Slither-Dogs
    Signed-off-by: AriDEV <michalik.andrej@centrum.sk>

--- Tools/Database_Installer.bat
+++ Tools/Database_Installer.bat
@@ -8,7 +8,7 @@ ECHO.
 ECHO          ษออออออออออออออออออออออออออออออออป
 ECHO          บ                                บ
 ECHO          บ        Welcome to the DB       บ
-ECHO          บ      SkyFireDB 406a Rev 201    บ
+ECHO          บ      SkyFireDB 406a Rev 202    บ
 ECHO          บ        Installation Tool       บ
 ECHO          บ                                บ
 ECHO          ศออออออออออออออออออออออออออออออออผ
@@ -208,6 +208,7 @@ ECHO   Here is a list of changesets.!!!)
 ECHO.   
 ECHO   changeset 200 = 200
 ECHO   changeset 201 = 201
+ECHO   changeset 202 = 202
 ECHO.
 ECHO   Or type in "A" to import all changesets
 ECHO.
@@ -219,6 +220,7 @@ IF %ch%==a GOTO changesetall
 IF %ch%==A GOTO changesetall
 IF %ch%==200 GOTO changeset200
 IF %ch%==201 GOTO changeset201
+IF %ch%==202 GOTO changeset202
 IF %ch%==b GOTO begin
 IF %ch%==B GOTO begin
 IF %ch%=="" GOTO changeset
@@ -239,6 +241,16 @@ ECHO.
 ECHO import: Changeset 201
 %mysqlpath%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < %changsql%\201_world_npc_trainer.sql
 ECHO Changeset 201 imported sucesfully!
+ECHO.
+PAUSE   
+GOTO changeset
+
+:changeset202
+CLS
+ECHO.
+ECHO import: Changeset 202
+%mysqlpath%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < %changsql%\202_world_creature_template.sql
+ECHO Changeset 202 imported sucesfully!
 ECHO.
 PAUSE   
 GOTO changeset
