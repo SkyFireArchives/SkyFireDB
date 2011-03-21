ede6097b5e21be09c82d8f9b151cc141e4e326a4
AriDEV <ari@nightandmagic.cz>
2011-03-21 14:23:46 -0700

    DB/Player: Rewrited Update 211, because somone dont know how to post patches !

--- Updates/211_world_newplayer_pets.sql
+++ Updates/211_world_newplayer_pets.sql
@@ -1,23 +1,26 @@
-Insert INTO playercreateinfo_spell Values (10, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (11, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (3, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (9, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (1, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (4, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (2, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (6, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (8, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (5, 3, 883, 'Call Pet 1');
-Insert INTO playercreateinfo_spell Values (22, 3, 883, 'Call Pet 1');
+DELETE FROM playercreateinfo_spell WHERE spell IN (883, 982);
+INSERT INTO playercreateinfo_spell VALUES
+(1, 3, 883, 'Call Pet 1'),
+(2, 3, 883, 'Call Pet 1'),
+(3, 3, 883, 'Call Pet 1'),
+(4, 3, 883, 'Call Pet 1'),
+(5, 3, 883, 'Call Pet 1'),
+(6, 3, 883, 'Call Pet 1'),
+(8, 3, 883, 'Call Pet 1'),
+(9, 3, 883, 'Call Pet 1'),
+(10, 3, 883, 'Call Pet 1'),
+(11, 3, 883, 'Call Pet 1'),
+(22, 3, 883, 'Call Pet 1');
 
-Insert INTO playercreateinfo_spell Values (10, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (11, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (3, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (9, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (1, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (4, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (2, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (6, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (8, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (5, 3, 982, 'Revive Pet');
-Insert INTO playercreateinfo_spell Values (22, 3, 982, 'Revive Pet');
+INSERT INTO playercreateinfo_spell VALUES
+(1, 3, 982, 'Revive Pet'),
+(2, 3, 982, 'Revive Pet'),
+(3, 3, 982, 'Revive Pet'),
+(4, 3, 982, 'Revive Pet'),
+(5, 3, 982, 'Revive Pet'),
+(6, 3, 982, 'Revive Pet'),
+(8, 3, 982, 'Revive Pet'),
+(9, 3, 982, 'Revive Pet'),
+(10, 3, 982, 'Revive Pet'),
+(11, 3, 982, 'Revive Pet'),
+(22, 3, 982, 'Revive Pet');
