bbb44a977c2d0c1e310ed60b08c2dbcf97759e5c
Davide <darkdevid96@live.it>
2011-03-30 18:36:27 +0200

    DB/CREATURE: Added KillCredit for Quest : The Westfall Brigade

--- Tools/Database_Installer_Updater.bat
+++ Tools/Database_Installer_Updater.bat
@@ -8,7 +8,7 @@ ECHO.
 ECHO          ษออออออออออออออออออออออออออออออออป
 ECHO          บ                                บ
 ECHO          บ        Welcome to the DB       บ
-ECHO          บ      SkyFireDB 406a Rev 281    บ
+ECHO          บ      SkyFireDB 406a Rev 282    บ
 ECHO          บ        Installation Tool       บ
 ECHO          บ                                บ
 ECHO          ศออออออออออออออออออออออออออออออออผ
@@ -208,6 +208,7 @@ ECHO   Here is a list of changesets.!!!)
 ECHO.   
 ECHO   changeset 280 = 280
 ECHO   changeset 281 = 281
+ECHO   changeset 282 = 282
 ECHO.
 ECHO   Or type in "A" to import all changesets
 ECHO.
@@ -219,6 +220,7 @@ IF %ch%==a GOTO changesetall
 IF %ch%==A GOTO changesetall
 IF %ch%==280 GOTO changeset280
 IF %ch%==281 GOTO changeset281
+IF %ch%==282 GOTO changeset282
 IF %ch%==b GOTO begin
 IF %ch%==B GOTO begin
 IF %ch%=="" GOTO changeset
@@ -239,6 +241,16 @@ ECHO
 ECHO import: Changeset 281
 %mysqlpath%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < %changsql%\281_gameobject_questrelation.sql
 ECHO Changeset 281 imported sucesfully!
+ECHO.
+PAUSE   
+GOTO changeset
+
+:changeset281
+CLS
+ECHO
+ECHO import: Changeset 282
+%mysqlpath%\mysql --host=%host% --user=%user% --password=%pass% --port=%port% %world_db% < %changsql%\282_creature_questrelation.sql
+ECHO Changeset 282 imported sucesfully!
 ECHO.
 PAUSE   
 GOTO changeset
