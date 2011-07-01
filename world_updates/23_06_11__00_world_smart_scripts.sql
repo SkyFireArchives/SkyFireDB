7f1603c54607881c1a0453d34f119a1c18124ca7
Skyfire <admin@projectskyfire.org>
2011-06-30 20:34:29 -0500

    Fix update files. event_param2 can not be -1 Did not see any issues after these changes.
    Signed-off-by: Skyfire <admin@projectskyfire.org>

--- world_updates/23_06_11__00_world_smart_scripts.sql
+++ world_updates/23_06_11__00_world_smart_scripts.sql
@@ -571,7 +571,7 @@ SET @ENTRY := 26472;
 UPDATE `creature_template` SET `AIName`='SmartAI' WHERE `entry`=@ENTRY;
 DELETE FROM `smart_scripts` WHERE `source_type`=0 AND `entryorguid`=@ENTRY;
 INSERT INTO `smart_scripts` (`entryorguid`,`source_type`,`id`,`link`,`event_type`,`event_phase_mask`,`event_chance`,`event_flags`,`event_param1`,`event_param2`,`event_param3`,`event_param4`,`action_type`,`action_param1`,`action_param2`,`action_param3`,`action_param4`,`action_param5`,`action_param6`,`target_type`,`target_param1`,`target_param2`,`target_param3`,`target_x`,`target_y`,`target_z`,`target_o`,`comment`) VALUES
-(@ENTRY,0,0,1,8,0,100,1,49319,-1,0,0,25,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Flee'),
+(@ENTRY,0,0,1,8,0,100,1,49319,0,0,0,25,0,0,0,0,0,0,1,0,0,0,0,0,0,0,'Cast Flee'),
 (@ENTRY,0,1,2,61,0,100,1,0,0,0,0,11,49323,0,0,0,0,0,7,0,0,0,0,0,0,0,'Highland Mustang Spooking Credit'),
 (@ENTRY,0,2,0,61,0,100,1,0,0,0,0,41,5000,0,0,0,0,0,1,0,0,0,0,0,0,0,'Despawn');
 
