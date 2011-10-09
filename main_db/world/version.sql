a8c38e3ae68e603b42792671d6933bd47c945bff
Skyfire <admin@projectskyfire.org>
2011-10-09 11:45:53 -0500

    Applied all the latest updates and changes to sync the database with the current core. Not much for content changes at this time.
    Signed-off-by: Skyfire <admin@projectskyfire.org>

--- main_db/world/version.sql
+++ main_db/world/version.sql
@@ -20,7 +20,7 @@ CREATE TABLE `version` (
 
 LOCK TABLES `version` WRITE;
 /*!40000 ALTER TABLE `version` DISABLE KEYS */;
-INSERT INTO `version` VALUES ('SkyFireEMU Rev: 1833 Release Hash: ede49248f733 (Win32,little-endian)','1833','SkyFireDB 406a-4.2.0a.08_18_2011 Rev 467','\"patch\" SmartAI Lab 420.03',0);
+INSERT INTO `version` VALUES ('SkyFireEMU Rev: 1998 Release Hash: ede49248f733 (Win32,little-endian)','1998','SkyFireDB 406a.10_09_2011 Rev 468','\"patch\" SmartAI Lab 420.03',0);
 /*!40000 ALTER TABLE `version` ENABLE KEYS */;
 UNLOCK TABLES;
 DELIMITER ;;
